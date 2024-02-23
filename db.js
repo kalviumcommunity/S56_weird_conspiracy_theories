ea9e2920309b4f64c09aa65eef7c459a6acab3b2
abhinav0306 <abhinavs0603@gmail.com>
2024-02-23 18:04:43 +0530

    mongoDB added

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose=require("mongoose")
+require("dotenv").config()
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.mongoURI,{
+        })
+        console.log("success in connecting to the database")
+    }catch(error){
+        console.error("failed to connect to the database")
+    }
+}
+let mongooseConnect=()=>{
+    return mongoose.connection.readyState===1?"connected successfully":"not connected"
+}
+module.exports={connectDB,mongooseConnect}
