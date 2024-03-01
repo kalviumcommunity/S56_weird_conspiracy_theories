286f3b7fa3cae7ae133fd81befcc72f18a5bf3c9
abhinav0306 <abhinavs0603@gmail.com>
2024-03-01 09:26:20 +0530

    nav.jsx updated

--- frontend/src/components/nav.jsx
+++ frontend/src/components/nav.jsx
@@ -1,8 +1,8 @@
 
 import React from 'react';
 import logo from "../assets/logo.png";
-import { Button } from '@mui/material/Button';
-import { TextField } from '@mui/material/TextField';
+import Button  from '@mui/material/Button';
+import TextField  from '@mui/material/TextField';
 import "./nav.css";
 
 const Nav = () => {
