5ad83c258b74d9cec98af4c23fc7349679aff079
abhinav0306 <abhinavs0603@gmail.com>
2024-05-04 16:57:07 +0530

    conflict res

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -17,19 +17,9 @@ router.put('/put', (req, res) => {
 router.delete('/delete', (req, res) => { 
   res.json('DELETE request');
 });
-
-<<<<<<< HEAD:backend/routes/routes.js
-
-// error handling for 404 requests.
-router.use((req, res) => {
-  res.status(404).json('404 Not Found');
-});
-
-=======
 // error handling for 404 requests.
 
 router.use((req, res) => {
   res.status(404).json('404 Not Found');
 });
->>>>>>> b7758a31706a0cb74e6130cf1df42db84d777aef:routes/routes.js
 module.exports = {router};
